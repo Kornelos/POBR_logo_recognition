2b331341ad071b50c47edca5f0286095951b8144
Kornel Skorka <kornel.skorka@allegro.pl>
2022-01-21 21:41:07 +0100

    finished (needs amends in recognition)

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from typing import Tuple
 import cv2
 import numpy as np
 
-from converters import bgr2hsv
+from converters import bgr2hsv, _bgr2hsv_pixel_fast
 from detection import flood_fill
-from processing import image_convolution, apply_threshold
+from processing import image_convolution, apply_threshold, dilate, erode
 from recognition import is_hm
+
+SHOW_THRESHOLD = True
 
 
 def merge_boxes(p0, p1, p2, p3) -> Tuple[Tuple[int, int], Tuple[int, int]]:
@@ -18,9 +20,8 @@ def merge_boxes(p0, p1, p2, p3) -> Tuple[Tuple[int, int], Tuple[int, int]]:
     return (j_min, i_min), (j_max, i_max)
 
 
-def detect_logo(path):
+def detect_logo(path, should_close):
     img = cv2.imread(path)
-    # img = img[100:300, 100:300]  # for development only
 
     # blur
     kernel = np.array([
@@ -32,21 +33,20 @@ def detect_logo(path):
     img2 = image_convolution(img, kernel)
 
     # to hsv
-    # img2_cv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     img2 = bgr2hsv(img2)
 
-    # print(_bgr2hsv_pixel_fast(np.array([201, 38, 53])))
     # threshold
-    img2 = apply_threshold(img2, np.uint8(171))
+    img2 = apply_threshold(img2)
 
-    # todo: closing (dilitate & erode) - maybe not necessary?
-    kernel = np.ones((3, 3), np.uint8)
-    # cv2.dilate(img2, kernel, img2)
-    # cv2.erode(img2, kernel, img2)
+    # closing (dilitate & erode) 3x3 kernel
+    if should_close:
+        img2 = dilate(img2)
+        img2 = erode(img2)
 
     # flood fill & boxing
     detected = flood_fill(img2)
     merge_queue = []
+    p1, p2 = (0, 0)
     for idx, d in enumerate(detected):
         p1, p2 = d[2]  # (j_min, i_min), (j_max, i_max)
         hm, should_merge = is_hm(matrix=d[0], pixel_count=d[1])
@@ -60,11 +60,19 @@ def detect_logo(path):
                 cv2.rectangle(img, p1, p2, color=(0, 255, 0))
             else:
                 merge_queue.append((p1, p2))
-    cv2.imshow("result", np.hstack((img, img2)))
+    if merge_queue:
+        cv2.rectangle(img, p1, p2, color=(0, 255, 0))
+
+    if SHOW_THRESHOLD:
+        cv2.imshow("result", np.hstack((img, img2)))
+    else:
+        cv2.imshow(path, img)
 
 
 if __name__ == '__main__':
     logo_path = "./images/hm-logo.jpeg"
-
-    detect_logo("./images/img1.jpeg")
+    # print(_bgr2hsv_pixel_fast(np.array([201, 38, 53]))) # experimenting with hsv ranges
+    # detect_logo("./images/img1.jpeg", False)
+    detect_logo("./images/img2.jpeg", False)
+    # detect_logo("./images/img3.jpeg", True)
     cv2.waitKey()
--- processing.py
+++ processing.py
@@ -19,9 +19,45 @@ def image_convolution(matrix: np.ndarray, kernel: np.ndarray):
 
 
 @njit
-def apply_threshold(matrix: np.ndarray, hue_value: np.uint8) -> np.ndarray:
+def dilate(m: np.ndarray):
+    m_height, m_width, channels = m.shape
+
+    # iterates through matrix, applies kernel, and sums
+    output = np.zeros(m.shape, dtype=np.uint8)
+    for i in range(1, m_height - 1):
+        for j in range(1, m_width - 1):
+            if m[i - 1][j - 1][0] == 255 or m[i - 1][j][0] == 255 or \
+                    m[i - 1][j + 1][0] == 255 or m[i][j - 1][0] == 255 or \
+                    m[i][j + 1][0] == 255 or m[i + 1][j - 1][0] == 255 or \
+                    m[i + 1][j][0] == 255 or m[i + 1][j + 1][0] == 255:
+                output[i][j] = np.uint(255)
+            else:
+                output[i][j] = np.uint(0)
+    return output
+
+
+@njit
+def erode(m: np.ndarray):
+    m_height, m_width, channels = m.shape
+
+    # iterates through matrix, applies kernel, and sums
+    output = np.zeros(m.shape, dtype=np.uint8)
+    for i in range(1, m_height - 1):
+        for j in range(1, m_width - 1):
+            if m[i - 1][j - 1][0] == 255 and m[i - 1][j][0] == 255 and \
+                    m[i - 1][j + 1][0] == 255 and m[i][j - 1][0] == 255 and \
+                    m[i][j + 1][0] == 255 and m[i + 1][j - 1][0] == 255 and m[i + 1][j][0] == 255 and \
+                    m[i + 1][j + 1][0] == 255:
+                output[i][j] = np.uint(255)
+            else:
+                output[i][j] = np.uint(0)
+    return output
+
+
+@njit
+def apply_threshold(matrix: np.ndarray) -> np.ndarray:
     m_height, m_width, channels = matrix.shape
-    output = np.zeros(matrix.shape, dtype=np.uint8)  # todo: change to 1D
+    output = np.zeros(matrix.shape, dtype=np.uint8)
     for i in range(m_height):
         for j in range(m_width):
             output[i][j] = _threshold_in_range(matrix[i][j])
@@ -30,10 +66,6 @@ def apply_threshold(matrix: np.ndarray, hue_value: np.uint8) -> np.ndarray:
 
 @njit
 def _threshold_in_range(pixel: np.ndarray) -> np.uint8:
-    # lower_red = np.array([160, 50, 50])
-    # upper_red = np.array([180, 255, 255])
-    # lower_red2 = np.array([0, 70, 50])
-    # upper_red2 = np.array([10, 255, 255])
     if (160 <= pixel[0] <= 180) and (100 <= pixel[1] <= 255) and (100 <= pixel[2] <= 255) or \
             (0 <= pixel[0] <= 10) and (100 <= pixel[1] <= 255) and (100 <= pixel[2] <= 255):
         return np.uint(255)
